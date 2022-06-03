c5744889832e76039afe84cb8eba5b2c5e01e1dd
MilanTheDeveloper@outlook.com <MilanTheDeveloper@outlook.com>
2022-06-03 18:11:53 +0200

    pitch additions

--- Platform/static/javascript/pitch.js
+++ Platform/static/javascript/pitch.js
@@ -1,12 +1,18 @@
 const tabs = ["pitch", "devlog", "faq", "about"];
 const tabCount = 4;
 
-var activeTab = 0;
+const devTabs = ["devHelp", "release0", "patch0-1"]
+const devTabsCount = 3;
 
 window.onload = function startup() {
   for (let i = 0; i < tabCount; i++) {
     document.querySelector('.' + tabs[i] + '-tab').style.display = 'none';
     document.querySelector('#' + tabs[i]).addEventListener('click', openTab)
+  }
+  
+  for (let j = 0; j < devTabsCount; j++) {
+    document.querySelector('.' + devTabs[j] + '-tab').style.display = 'none';
+    document.querySelector('#' + devTabs[j]).addEventListener('click', openDevTab);
   }
 }
 
@@ -14,9 +20,21 @@ const openTab = (e) => {
   if (tabs.includes(e.target.id)) {
     for (let i = 0; i < tabCount; i++) {
       if (tabs[i] == e.target.id) {
-        document.querySelector('.' + tabs[i] + '-tab').style.display = 'block';
+        document.querySelector('.' + tabs[i] + '-tab').style.display = '';
     } else {
       document.querySelector('.' + tabs[i] + '-tab').style.display = 'none';
+      }
+    }
+  }
+}
+
+const openDevTab = (e) => {
+  if (devTabs.includes(e.target.id)) {
+    for (let i = 0; i < devTabsCount; i++) {
+      if (devTabs[i] == e.target.id) {
+        document.querySelector('.' + devTabs[i] + '-tab').style.display = '';
+      } else {
+        document.querySelector('.' + devTabs[i] + '-tab').style.display = 'none';
       }
     }
   }
